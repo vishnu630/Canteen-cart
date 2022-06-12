2416c35558dddbed7524dd892dc597d195aa4988
vishnu teja reddy <vishnuteja779986@gmail.com>
2022-06-12 17:34:04 +0530

    Initial commit

---
+++ src/components/Recipe.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+//import { addShipping } from './actions/cartActions'
+class Recipe extends Component{
+    
+    componentWillUnmount() {
+         if(this.shipping.checked)
+              this.props.substractShipping()
+    }
+
+    handleChecked = (e)=>{
+        if(e.target.checked){
+            this.props.addShipping();
+        }
+        else{
+            this.props.substractShipping();
+        }
+    }
+
+    render(){
+  
+        return(
+            <div className="container">
+                <div className="collection">
+                        <li className="collection-item"><b>Total: Rs {this.props.total}</b></li>
+                    </div>
+                    <div className="checkout">
+                        <button className="waves-effect waves-light btn">Checkout</button>
+                    </div>
+                 </div>
+        )
+    }
+}
+
+const mapStateToProps = (state)=>{
+    return{
+        addedItems: state.addedItems,
+        total: state.total
+    }
+}
+
+const mapDispatchToProps = (dispatch)=>{
+    return{
+        addShipping: ()=>{dispatch({type: 'ADD_SHIPPING'})},
+        substractShipping: ()=>{dispatch({type: 'SUB_SHIPPING'})}
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Recipe)
